8aac2cb9ae14b68ed68b06999ecf7c73ee415bc8
Georgyi <112905032+georgij-tsabiev@users.noreply.github.com>
2024-11-16 00:26:43 +0300

    Исправляет стилизацию и разметку попапа

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,23 +1,21 @@
 const initModalHandler = () => {
   const modalNode = document.querySelector('.modal');
-  const openBtnNode = document.querySelector('.sign-up-button')
-  const closeBtnNode = document.querySelector('.modal__close-button');
+  const openBtnNode = document.querySelector('.sign-up-button');
+  const toggleModal = (isVisible) => {
+    modalNode.style.display = isVisible ? 'flex' : 'none';
+  };
   openBtnNode.addEventListener('click', (event) => {
     event.preventDefault();
-    modalNode.style.display = 'block';
-  });
-  closeBtnNode.addEventListener('click', (event) => {
-    event.preventDefault();
-    modalNode.style.display = 'none';
+    toggleModal(true);
   });
   window.addEventListener('click', (event) => {
-    if (event.target == modalNode) {
-      modalNode.style.display = 'none';
+    if (event.target === modalNode) {
+      toggleModal(false);
     }
   });
   document.addEventListener('keydown', (event) => {
     if (event.key === 'Escape') {
-      modalNode.style.display = "none";
+      toggleModal(false);
     }
   });
 };
